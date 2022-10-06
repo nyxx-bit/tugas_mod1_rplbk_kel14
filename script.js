b47d507e93139d020b7bcd9d876bf9855517ccff
haannzzeell <hanaadilahh@gmail.com>
2022-10-06 23:29:34 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1444,3 +1444,15 @@ let data = [
       },
     ],
   ];
+  function filterData(e) {
+    const element = data[0].filter((el) =>
+      el.nim.toString().toLowerCase().includes(e.value.toLowerCase())
+    );
+    let wrapper = document.getElementById("wrapper-element");
+  
+    if (e.value === "" || element.length === 0) {
+      wrapper.innerHTML = "There is nothing in here";
+    } else {
+      renderData(element);
+    }
+  }
